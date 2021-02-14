a8c17d434dba3b2a0453cfddcc03385623a8f132
Avinash Kumar <avirms07@gmail.com>
2021-02-14 19:49:04 +0530

    Add files via upload

---
+++ Search & sort/alternativesorting.cpp
@@ -0,0 +1,19 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int n;
+    cin>>n;
+    int a[n];
+    for(int i=0;i<n;i++)
+        cin>>a[i];
+    sort(a,a+n);
+    int i=0,j=n-1;
+    while(i<j)
+    {
+        cout<<a[j--]<<" ";
+        cout<<a[i++]<<" ";
+    }
+    if(n%2!=0)
+        cout<<a[i];
+}
