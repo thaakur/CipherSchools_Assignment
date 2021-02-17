4973099685685ad4da52dba6d8cd6835a6e7dbcf
Avinash Kumar <avirms07@gmail.com>
2021-02-18 02:20:49 +0530

    Update generateparanthesis.cpp

--- Recursion and Backtracking/generateparanthesis.cpp
+++ Recursion and Backtracking/generateparanthesis.cpp
@@ -12,17 +12,13 @@ void backtrack(vector<string> ans,string curr,int open,int close,int maxm)
     if(close<open)
         backtrack(ans,curr+")",open,close+1,maxm);
 }
-vector<string> generateparanthesis(int n){
+void generateparanthesis(int n){
     vector<string> ans;
     backtrack(ans,"",0,0,n);
-    return ans;
 }
 int main()
 {
     int n;
     cin>>n;
-    vector<string> v(2*n);
-    v=generateparanthesis(n);
-    for(int i=0;i<v.size();i++)
-        cout<<v[i]<<endl;
+    generateparanthesis(n);
 }
