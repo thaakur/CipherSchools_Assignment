04c9fd3252b761130d52070aa0020d216be13429
Avinash Kumar <avirms07@gmail.com>
2021-06-24 01:46:12 +0530

    Update majorityElement.cpp

--- Day 1 22 June/majorityElement.cpp
+++ Day 1 22 June/majorityElement.cpp
@@ -1,1 +1,25 @@
 
+class Solution {
+public:
+    int majorityElement(vector<int>& nums) {
+        int cnt=1;
+        int n=nums.size();
+        int maj=nums[0];
+        for(int i=1;i<n;i++)
+        {
+            if(maj==nums[i])
+            {
+                cnt++;
+            }
+            else
+            {
+                cnt--;
+            }
+            if(cnt==0){
+                cnt++;
+                maj=nums[i]; 
+            }
+        }
+        return maj;
+    }
+};
