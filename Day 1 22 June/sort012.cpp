60738ee349ee390a87ffa4f74f80c42e08862553
Avinash Kumar <avirms07@gmail.com>
2021-06-24 01:49:30 +0530

    Update sort012.cpp

--- Day 1 22 June/sort012.cpp
+++ Day 1 22 June/sort012.cpp
@@ -1,1 +1,23 @@
 
+class Solution {
+public:
+    void sortColors(vector<int>& nums) {
+        int mid=0,l=0,h=nums.size()-1;
+        while(mid<=h)
+        {
+            if(nums[mid]==0)
+            {
+                swap(nums[mid],nums[l]);
+                l++;
+                mid++;
+            }
+            else if(nums[mid]==1)
+                mid++;
+            else
+            {
+                swap(nums[mid],nums[h]);
+                h--;
+            }
+        }
+    }
+};
