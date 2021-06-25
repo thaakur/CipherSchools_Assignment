c91fb4d7e8dcfd4158b620456a20d4422571e4e2
Avinash Kumar <avirms07@gmail.com>
2021-06-26 05:18:31 +0530

    Update twoPeopleMeetEachOther.cpp

--- Day 1 22 June/twoPeopleMeetEachOther.cpp
+++ Day 1 22 June/twoPeopleMeetEachOther.cpp
@@ -1,1 +1,24 @@
+class solution
+{
+  public:
+  bool everMeet(int x1, int x2, int v1, int v2)
+  {
+      if (x1 < x2 && v1 <= v2)
+         return false;
+      if (x1 > x2 && v1 >=v2)
+         return false; 
+      if (x1 < x2)
+      {
+          swap(x1, x2);
+          swap(v1, v2);
+      }    
+      while (x1 >= x2) {
+          if (x1 == x2)
+              return true;
 
+          x1 = x1 + v1; 
+          x2 = x2 + v2;
+      }
+      return false;  
+  }
+};
