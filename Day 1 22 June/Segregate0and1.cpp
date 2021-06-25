8d0cb6fcb7c4532df323503d69a50346214548d6
Avinash Kumar <avirms07@gmail.com>
2021-06-26 05:27:51 +0530

    Update Segregate0and1.cpp

--- Day 1 22 June/Segregate0and1.cpp
+++ Day 1 22 June/Segregate0and1.cpp
@@ -1,1 +1,20 @@
 
+class Solution{   
+public:
+    void segregate0and1(int arr[], int n) {
+        // code here
+        int i=-1,j=n;
+        while(1)
+        {
+            do{
+                i++;
+            }while(arr[i]==0);
+            do{
+                j--;
+            }while(arr[j]==1);
+            if(i>j)
+                return;
+            swap(arr[i],arr[j]);
+        }
+    }
+};
