b39d355da1066f8a2d9f0a4c64e5e201b6ec2ff9
Avinash Kumar <avirms07@gmail.com>
2021-06-26 05:23:23 +0530

    Update alternativeSorting.cpp

--- Day 1 22 June/alternativeSorting.cpp
+++ Day 1 22 June/alternativeSorting.cpp
@@ -1,1 +1,17 @@
+class Solution{
+	public:
+	vector<int> alternateSort(int arr[], int N)
+	{
+	    vector<int> v;
+	    sort(arr,arr+N);
+	    for(int i=0,j=N-1;i<=j;i++,j--)
+	    {
+	        if(i!=j)
+	            v.push_back(arr[j]);
+	        v.push_back(arr[i]);
+	    }
+	    return v;
+	    // Your code goes here
+	}
 
+};
