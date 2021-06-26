ff152020675c15a3c85964038390824d66974cee
Avinash Kumar <avirms07@gmail.com>
2021-06-26 05:30:03 +0530

    Update PeakElement.cpp

--- Day 1 22 June/PeakElement.cpp
+++ Day 1 22 June/PeakElement.cpp
@@ -1,1 +1,21 @@
 
+class Solution
+{
+    public:
+    int peakElement(int arr[], int n)
+    {
+        int l=0,h=n-1;
+        while(l<=h)
+        {
+            int mid=l+(h-l)/2;
+            if((mid==0 || arr[mid-1]<=arr[mid]) && (mid==n-1 || arr[mid+1]<=arr[mid]))
+                return mid;
+            else if(mid>0 && arr[mid-1]>=arr[mid])
+                h=mid-1;
+            else 
+                l=mid+1;
+        }
+        return -1;
+       // Your code here
+    }
+};
