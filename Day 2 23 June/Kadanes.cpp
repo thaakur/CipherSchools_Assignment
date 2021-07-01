d3ab5b0a38f41900b61e7a33c49382ad2d480ee1
Avinash Kumar <avirms07@gmail.com>
2021-07-01 15:53:31 +0530

    Update Kadanes.cpp

--- Day 2 23 June/Kadanes.cpp
+++ Day 2 23 June/Kadanes.cpp
@@ -1,1 +1,13 @@
 
+class Solution {
+public:
+    int maxSubArray(vector<int>& nums) {
+        int maxEnd=nums[0],res=nums[0];
+        for(int i=1;i<nums.size();i++)
+        {
+            maxEnd=max(maxEnd+nums[i],nums[i]);
+            res=max(res,maxEnd);
+        }
+        return res;
+    }
+};
