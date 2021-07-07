ef1e261666c93b76ec618724894a73725c2109b6
Avinash Kumar <avirms07@gmail.com>
2021-07-07 23:10:35 +0530

    Update countPathFromTopLeftToBottomRight.cpp

--- Day 3-4 24-25 June/countPathFromTopLeftToBottomRight.cpp
+++ Day 3-4 24-25 June/countPathFromTopLeftToBottomRight.cpp
@@ -1,1 +1,23 @@
+class Solution {
+public:
+    int countPath(int row, int col){
+        vector<vector<int>> paths(row,vector<int>(col,0));
+        for(int i=0;i<row;i++)
+        {
+            paths[i][0]=1;
+        }
+        for(int i=0;i<col;i++)
+        {
+            paths[0][i]=1;
+        }
+        for(int i=1;i<row;i++)
+        {
+            for(int j=1;j<col;j++)
+            {
+                paths[i][j]= paths[i-1][j] + paths[i][j-1];
+            }
+        }
+        return paths[row-1][col-1];
+    }
 
+};
