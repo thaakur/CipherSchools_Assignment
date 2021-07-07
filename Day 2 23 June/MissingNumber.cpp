e5310ba937e2a0d8c6a30a75a7bccebf6f5d253d
Avinash Kumar <avirms07@gmail.com>
2021-07-07 22:48:55 +0530

    Update MissingNumber.cpp

--- Day 2 23 June/MissingNumber.cpp
+++ Day 2 23 June/MissingNumber.cpp
@@ -1,1 +1,12 @@
 
+class Solution {
+public:
+    int findMissing(vector<int> &arr){
+        int size = arr.size();
+        int sum = (size + 1) * (size + 2) / 2;
+        for(int i : arr){
+            sum -= i;
+        }
+        return sum;
+    }
+};
