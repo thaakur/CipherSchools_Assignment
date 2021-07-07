f64b2670a51843e8f476e1697d7bb3162dd8bd99
Avinash Kumar <avirms07@gmail.com>
2021-07-07 23:05:50 +0530

    Update generateAllPossibleParenthesis.cpp

--- Day 3-4 24-25 June/generateAllPossibleParenthesis.cpp
+++ Day 3-4 24-25 June/generateAllPossibleParenthesis.cpp
@@ -1,1 +1,35 @@
 
+class Solution {
+public:
+    void generateParenthesisHelper(int open, int close, string &resultSoFar, vector<string> &result){
+        if(open < 0 || close < 0 ){
+            return;
+        }
+        if(open>close)
+        {
+            return;
+        }
+        else{
+            if(open==0 && close ==0)
+            {
+                result.push_back(resultSoFar);
+                return;
+            }
+
+            resultSoFar.push_back('(');
+            generateParenthesisHelper(open-1, close, resultSoFar, result);
+            resultSoFar.pop_back();
+            resultSoFar.push_back(')');
+            generateParenthesisHelper(open,close-1, resultSoFar,result);
+            resultSoFar.pop_back();
+
+        }
+    }
+    vector<string> generateParentheses(int N)
+    {
+        vector<string> result;
+        string resultSoFar = "";
+        generateParenthesisHelper(N,N,resultSoFar,result);
+        return result;
+    }
+};
