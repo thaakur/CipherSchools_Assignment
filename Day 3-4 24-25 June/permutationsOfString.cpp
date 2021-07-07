617fac851a8b86ac9653386c04ba5f6eba4fa2bd
Avinash Kumar <avirms07@gmail.com>
2021-07-07 23:12:17 +0530

    Update permutationsOfString.cpp

--- Day 3-4 24-25 June/permutationsOfString.cpp
+++ Day 3-4 24-25 June/permutationsOfString.cpp
@@ -1,1 +1,22 @@
 
+class Solution {
+public:
+    void generateStringPermutationHelper(string &str, int start, int end, vector<string> &result){
+        if(start>=end){
+            result.push_back(str);
+            return;
+        }
+        for(int i=start; i<=end;i++)
+        {
+            swap(str[i],str[start]);
+            generateStringPermutationHelper(str,start+1,end,result);
+            swap(str[i], str[start]);
+        }
+    }
+    vector<string> generateStringPermutation(string &str){
+        vector<string> result;
+        generateStringPermutationHelper(str,0,str.size()-1,result);
+        return result;
+    }
+
+};
