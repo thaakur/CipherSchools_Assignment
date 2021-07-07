c194f04586996547450df81c811570582d954877
Avinash Kumar <avirms07@gmail.com>
2021-07-07 23:16:44 +0530

    Create trailingZeros.cpp

---
+++ Practice Sheet 4/trailingZeros.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int countTrailingZeroes(int n){
+        if(n<0)
+            return 0;
+        int cnt = 0;
+        for(int i= 5 ; n / i >= 1; i*=5)
+        {
+            cnt += n/i;
+        }
+        return cnt;
+    }
+};
