0fc6674e48ccd53f0c5d4a7cc26517c03da2563a
Avinash Kumar <avirms07@gmail.com>
2021-07-07 23:06:33 +0530

    Update lastNonZeroDigitFactorial.cpp

--- Day 3-4 24-25 June/lastNonZeroDigitFactorial.cpp
+++ Day 3-4 24-25 June/lastNonZeroDigitFactorial.cpp
@@ -1,1 +1,14 @@
-
+class Solution {
+public:
+    int digit[] = {1, 1, 2, 6, 4, 2, 2, 4, 2, 8};
+     
+    int lastNonZeroInFactorial(int n)
+    {
+         if (n < 10)
+            return digit[n];
+        if (((n/10)%10)%2 == 0) //second last digits i.e 10s place is even or not
+            return (6*lastNonZeroInFactorial(n/5)*digit[n%10]) % 10;
+        else
+            return (4*lastNonZeroInFactorial(n/5)*digit[n%10]) % 10;
+    }
+};
