58aeaf25972bba6843a98a135782a25cef34c47c
Avinash Kumar <avirms07@gmail.com>
2021-07-07 23:11:23 +0530

    Update subsetSum.cpp

--- Day 3-4 24-25 June/subsetSum.cpp
+++ Day 3-4 24-25 June/subsetSum.cpp
@@ -1,1 +1,30 @@
 
+class Solution {
+public:
+    int isPartitionSubset(int arr[], int n, int sum){
+        if (sum == 0)
+            return 1;
+        if (n == 0 && sum != 0)
+            return 0;
+     
+        if (arr[n - 1] > sum)
+            return isPartitionSubset(arr, n - 1, sum);
+     
+        return isPartitionSubset(arr, n - 1, sum)
+               || isPartitionSubset(arr, n - 1, sum - arr[n - 1]);
+    }
+        
+    int equalPartition(int n, int arr[])
+    {
+        int sum =0;
+        for(int i=0;i<n;i++)
+        {
+            sum+=arr[i];
+        }
+        if(sum%2==1)
+            return 0;
+        sum = sum / 2;
+        return isPartitionSubset(arr, n, sum);
+    }
+
+};
