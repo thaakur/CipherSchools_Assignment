60e5cadf0fa08493571828aad817da82d5d8392f
Avinash Kumar <avirms07@gmail.com>
2021-07-07 22:51:53 +0530

    Update StockBuySell.cpp

--- Day 2 23 June/StockBuySell.cpp
+++ Day 2 23 June/StockBuySell.cpp
@@ -1,1 +1,25 @@
 
+class Solution {
+public:
+    int maxProfit(vector<int> prices)
+    {
+        int n = prices.size();
+        int cost = 0;
+        int maxCost = 0;
+     
+        if (n == 0) 
+        {
+            return 0;
+        }
+        
+        int min_price = prices[0];
+     
+        for(int i = 0; i < n; i++)
+        {
+            min_price = min(min_price, prices[i]);
+            cost = prices[i] - min_price;
+            maxCost = max(maxCost, cost);
+        }
+        return maxCost;
+    }
+};
