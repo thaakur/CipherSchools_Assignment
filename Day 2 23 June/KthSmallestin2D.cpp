ad37e7ded2ad27cbedc93d1931a0204f635d6d15
Avinash Kumar <avirms07@gmail.com>
2021-07-07 23:01:38 +0530

    Update KthSmallestin2D.cpp

--- Day 2 23 June/KthSmallestin2D.cpp
+++ Day 2 23 June/KthSmallestin2D.cpp
@@ -1,1 +1,42 @@
 
+class Solution {
+public:
+    int greaterThanOrEqual(int num, vector<vector<int>> mat){
+        int result = 0;
+
+        for(int i=0;i<mat.size();i++)
+        {
+            if(mat[i][0]>num)
+                return result;
+
+            if(mat[i][mat.size()-1]<=num){
+                result += mat.size();
+                continue;
+            }
+
+            int greaterThan = 0;
+            for( int j = mat.size()/2; j>=1; j/=2){
+                while(greaterThan + j < mat.size() && 
+                       mat[i][greaterThan+ j] <= num){
+                           greaterThan += j;
+                       }
+            }
+            result += greaterThan + 1;
+        }
+        return result;
+    }
+
+    int KthSmallest( vector<vector<int>> mat, int k){
+        int l = mat[0][0], r= mat[mat.size()-1][mat[0].size()-1];
+
+        while(l<=r){
+            int mid = l + (r - l) / 2;
+            int greaterThanOrEqualToMid = greaterThanOrEqual(mid, mat);
+            if(greaterThanOrEqualToMid >= k)
+            r =mid -1;
+            else
+            l = mid + 1;
+        }
+        return l;
+    }
+};
